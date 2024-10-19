45128da580ad330993014ae67f4530b42b1d6daf
ihnateskuyuliia <yuliiaihnatesku@uchicago.edu>
2024-10-19 18:33:20 +0000

    one

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" #not to shut down the server
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
 end
